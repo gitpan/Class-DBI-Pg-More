142ea9339d0313be30f7df2bd9d2f35661874569
Boris Sukholitko <boriss@gmail.com>
2007-01-22 21:54:59 +0000

    Import of BOSU/Class-DBI-Pg-More-0.01 from CPAN.
    gitpan-cpan-distribution: Class-DBI-Pg-More
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BOSU/Class-DBI-Pg-More-0.01.tar.gz
    gitpan-cpan-author:       BOSU
    gitpan-cpan-maturity:     released

---
+++ lib/Class/DBI/Pg/More.pm
@@ -0,0 +1,129 @@
+=head1 NAME
+
+Class::DBI::Pg::More - Enhances Class::DBI::Pg with more goodies.
+
+=head1 SYNOPSIS
+
+   package MyClass;
+   use base 'Class::DBI::Pg::More';
+   
+   __PACKAGE__->set_up_table("my_table");
+
+   # a_date is a date column in my_table. 
+   # Class::DBI::Plugin::DateFormat::Pg->has_date has been
+   # called for a_date implicitly.
+   my $a_date_info =  __PACKAGE__->pg_column_info('a_date')
+   print $a_date_info->{type}; # prints "date"
+
+   # an_important is an important column in my_table set to not null
+   print $a_date_info->{is_nullable} ? "TRUE" : "FALSE"; # prints FALSE
+
+=head1 DESCRIPTION
+
+This class overrides Class::DBI::Pg C<set_up_table> method to setup more
+things from the database.
+
+It recognizes date, timestamp etc. columns and calls
+C<Class::DBI::Plugin::DateTime::Pg> has_* methods on them.
+
+It also fetches some constraint information (currently C<not null>).
+
+=cut
+
+use strict;
+use warnings FATAL => 'all';
+
+package Class::DBI::Pg::More;
+use base 'Class::DBI::Pg';
+
+our $VERSION = '0.01';
+
+sub _handle_pg_datetime {
+	my ($class, $col, $type) = @_;
+	my $func;
+	if ($type eq 'date') {
+		$func = "has_$type";
+	} elsif ($type =~ /^(time\w+)/) {
+		$func = "has_$1";
+		$func .= "tz" unless $type =~ /without time zone/;
+	} else {
+		return;
+	}
+	eval "use Class::DBI::Plugin::DateTime::Pg";
+	die "Unable to use CDBIP::DT::Pg: $@" if $@;
+	$class->$func($col);
+}
+
+=head1 METHODS
+
+=head2 $class->set_up_table($table, $args)
+
+This is main entry point to the module. Please see C<Class::DBI::Pg>
+documentation for its description.
+
+This class automagically uses Class::DBI::Plugin::DateTime::Pg for date/time
+fields, so you should use DateTime values with them.
+
+=cut
+sub set_up_table {
+	my ($class, $table, $args) = @_;
+	$class->SUPER::set_up_table($table);
+
+	my %infos;
+	my $arr = $class->db_Main->selectall_arrayref(<<ENDS
+SELECT column_name, data_type, is_nullable FROM information_schema.columns
+	WHERE table_name = ?
+ENDS
+		, undef, $table);
+	for my $a (@$arr) {
+		my $i = { type => $a->[1] };
+		$class->_handle_pg_datetime($a->[0], $a->[1]);
+		$i->{is_nullable} = 1 if $a->[2] eq 'YES';
+		$infos{ $a->[0] } = $i;
+	}
+	$class->mk_classdata("Pg_Column_Infos", \%infos);
+}
+
+=head2 $class->pg_column_info($column)
+
+Returns column information as HASHREF. Currently supported flags are:
+
+=over
+
+=item type - Returns data type of the column (e.g. integer, text, date etc.).
+
+=item is_nullable - Indicates whether the C<$column> can be null.
+
+=back
+
+=cut
+sub pg_column_info {
+	my ($class, $col) = @_;
+	return $class->Pg_Column_Infos->{ $col };
+}
+
+1;
+
+=head1 AUTHOR
+
+	Boris Sukholitko
+	CPAN ID: BOSU
+	
+	boriss@gmail.com
+	
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+C<Class::DBI::Pg>, C<Class::DBI::Plugin::DateTime::Pg>.
+
+=cut
+
---
+++ t/001_load.t
@@ -0,0 +1,46 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More tests => 15;
+use Test::TempDatabase;
+
+BEGIN { use_ok( 'Class::DBI::Pg::More' ); }
+
+my $tdb = Test::TempDatabase->create(dbname => 'ht_class_dbi_test',
+		dbi_args => { RootClass => 'DBIx::ContextualFetch' });
+my $dbh = $tdb->handle;
+$dbh->do('SET client_min_messages TO error');
+$dbh->do("CREATE TABLE table1 (id serial primary key, d1 date
+		, d2 timestamp default now())");
+
+package T1;
+use base 'Class::DBI::Pg::More';
+sub db_Main { return $dbh; }
+
+package main;
+
+is(T1->can('has_date'), undef);
+
+T1->set_up_table('table1');
+is_deeply([ sort T1->columns ], [ qw(d1 d2 id) ]);
+isnt(T1->can('has_date'), undef);
+
+my $id_i = T1->pg_column_info('id');
+isnt($id_i, undef);
+is($id_i->{is_nullable}, undef);
+is($id_i->{type}, 'integer');
+
+my $d1_i = T1->pg_column_info("d1");
+isnt($d1_i->{is_nullable}, undef);
+is($d1_i->{type}, 'date');
+
+my $d2_i = T1->pg_column_info("d2");
+isnt($d2_i->{is_nullable}, undef);
+is($d2_i->{type}, 'timestamp without time zone');
+
+my $obj = T1->create({ d1 => DateTime->new(year => 1990, month => 8
+					, day => 12) });
+isnt($obj, undef);
+is($obj->d1->year, 1990);
+isnt($obj->d2, undef);
+is($obj->d2->minute, DateTime->now->minute);
